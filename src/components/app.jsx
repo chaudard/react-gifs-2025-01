5c4d231b3d4f085ad6a750d3260bc52c12c1cfae
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-03 17:02:10 +0100

    divide left div into search div on top and selectedGif div at the bottom

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const App = () => {
     return(
         <div className="flex-container">
-            <div id='flex-item-left'></div>
+            <div id='flex-item-left'>
+                <div id='search'></div>
+                <div id='selectedGif'></div>
+            </div>
             <div id='flex-item-right'></div>
         </div>
     )
