8268d4a7bcf4a4fa2193b295a94b7f4dc1150617
MICHEL Dany <donnetonfalzard+gitlab@qmail.com>
2025-01-03 12:34:11 +0100

    delete unused RandomImage component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import RandomImage from './randomImage';
 
 const App = () => {
     return(
         <div className="app">
-            <h1 className="text-center">First react app</h1>
-            <RandomImage />
+
         </div>
     )
 }
